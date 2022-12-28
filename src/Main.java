35fd22f4c7b04f03ff9b56a5fbe2612809df58e0
S <77491046+sviegasdc@users.noreply.github.com>
2022-12-28 20:33:36 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) throws InvalidNoException {
-        Arvore arvore = new  Arvore("Mr catra");
-        No n = arvore.addFilho(arvore.raiz, "adotado");
-        No a = arvore.addFilho(arvore.raiz, "junin");
-        No p = arvore.addFilho(arvore.raiz, "ovo");
-        No r = arvore.addFilho(n, "filho de n");
-//        arvore.mostraArvore();
-        System.out.println(arvore.size());
-        arvore.elements();
-        System.out.println(arvore.Nos());
-        System.out.println("altura da árvore: " + arvore.altura());
-        System.out.println("profundidade do no escolhido: " + arvore.profundidade(r));
 
     }
 }
